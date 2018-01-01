70d48c6ed30225fc82608da94b70d6038ea14d6c
Sevenfm <>
2018-01-01 00:32:53 +0000

    Build 18.01.01

--- GameVersion.cpp
+++ GameVersion.cpp
@@ -55,7 +55,7 @@
 
 #endif
 										
-CHAR8		czVersionNumber[16]	= { "Build 17.12.02" };		//YY.MM.DD
+CHAR8		czVersionNumber[16]	= { "Build 18.01.01" };		//YY.MM.DD
 CHAR16		zTrackingNumber[16]	= { L"Z" };
 	
 // SAVE_GAME_VERSION is defined in header, change it there
